93f032bac66f7fa9b6f1bf2fa618321b28b802c3
Mauricio Maldonado Iturriago <miturriago@uninorte.edu.co>
2021-02-15 12:23:51 -0500

    controller added

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,2 +1,39 @@
 class CustomersController < ApplicationController
+    def index
+        @customers = Customer.all 
+        render json: @customers
+    end 
+
+    def show
+        @customer = Customer.find(params[:id])
+        render json: @customer
+    end 
+
+    def create
+        @customer = Customer.create(
+            name: params[:name],
+            phone: params[:phone],
+            email: params[:email],
+            description: params[:description]
+        )
+        render json: @customer
+    end 
+
+    def update
+        @customer = Customer.find(params[:id])
+        @customer.update(
+            name: params[:name],
+            phone: params[:phone],
+            email: params[:email],
+            description: params[:description]
+        )
+        render json: @customer
+    end 
+
+    def destroy
+        @customers = Customer.all 
+        @customer = Customer.find(params[:id])
+        @customer.destroy
+        render json: @customers
+    end 
 end
